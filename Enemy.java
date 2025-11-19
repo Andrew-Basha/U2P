ef9f3592ec4b0635f786388564a15615d557bc67
Andrew-Basha <Andrewb298@nycstudents.net>
2025-11-19 08:45:14 -0500

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -14,6 +14,12 @@ public class Enemy {
         speed = enemySpd;
     }
 
+    int atkBuff = 0;
+    int atkBuffTimer = 0;
+    int defBuff = 0;
+    int defBuffTimer = 0;
+    int spdBuff = 0;
+    int spdBuffTimer = 0;
     public String getName(){
         return name;
     }
@@ -35,6 +41,30 @@ public class Enemy {
     }
 
     public int takeTurn(String AI, int playerDef){
+        if (atkBuff > 0) {
+            if (atkBuffTimer > 0) {
+                atkBuffTimer--;
+            } else {
+                attack -= atkBuff;
+                atkBuff = 0;
+            }
+        }
+        if (defBuff > 0) {
+            if (defBuffTimer > 0) {
+                defBuffTimer--;
+            } else {
+                defense -= defBuff;
+                defBuff = 0;
+            }
+        }
+        if (spdBuff > 0) {
+            if (spdBuffTimer > 0) {
+                spdBuffTimer--;
+            } else {
+                speed -= spdBuff;
+                spdBuff = 0;
+            }
+        }
         int dmgTaken;
         int recoil;
         double pickAttack = Math.random(); //"random" element
@@ -64,7 +94,49 @@ public class Enemy {
                 }
             }
         } else if (AI.equals("spiritType")) {
-            //To be implemented
+            dmgTaken = 0;
+            if (pickAttack < 0.4) {
+                dmgTaken = attack - playerDef;
+                System.out.println(name + " attacks\nplayer takes " + dmgTaken + " damage");
+            } else if (pickAttack < 0.5 && atkBuff == 0) {
+                attack += 2;
+                atkBuff = 2;
+                atkBuffTimer = 3;
+                System.out.println(name + " boosts its attack damage");
+            } else if (pickAttack < 0.5) {
+                atkBuffTimer = 3;
+                System.out.println(name + " lengthens its attack boost");
+            } else if (pickAttack < 0.6 && defBuff == 0) {
+                defense += 2;
+                defBuff = 2;
+                defBuffTimer = 3;
+                System.out.println(name + " boosts its ability to resist damage");
+            } else if (pickAttack < 0.6) {
+                defBuffTimer = 3;
+                System.out.println(name + " lengthens its resistance to damage");
+            } else if (pickAttack < 0.65) {
+                dmgTaken = (attack * 3) - playerDef;
+                System.out.println(name + " attacks with tremendous power\nplayer takes " + dmgTaken + " damage");
+            } else if (pickAttack < 0.9) {
+                System.out.println(name + " heals 3 HP from itself");
+                currentHP += 3;
+                if (currentHP > maxHP) {
+                    currentHP = maxHP;
+                }
+            } else {
+                System.out.println(name + " waits");
+            }
+        } else if (AI.equals("dragonType")) {
+            if (pickAttack < 0.7) {
+                dmgTaken = attack - playerDef;
+                System.out.println(name + " attacks\nplayer takes " + dmgTaken + " damage");
+            } else if (pickAttack < 0.95) {
+                dmgTaken = (int) (attack * 1.5) - playerDef;
+                System.out.println(name + " slashes at player\nplayer takes " + dmgTaken + " damage");
+            } else {
+                dmgTaken = (attack * 3) - playerDef;
+                System.out.println(name + " attacks with tremendous power\nplayer takes " + dmgTaken + " damage");
+            }
         } else {
             dmgTaken = 0;
         }
--- Runner.java
+++ Runner.java
@@ -1,9 +1,9 @@
 public class Runner {
     public static void main(String[] args){
-        double seed = Math.random();
-        Player player = new Player(20, 8, 3,0,4);
-        Enemy enemy1 = new Enemy("Wolf", 8, 3, 0, 2);
-        String AI = "beastType";
+        double random = Math.random();
+        Player player = new Player(20, 8, 3,0,3);
+        Enemy enemy1 = new Enemy("Green Slime", 5, 2, 0, 3);
+        String AI = "slimeType";
         System.out.println(enemy1.getName() + " appeared");
         int dmgTaken;
         while(enemy1.getCurrentHP() > 0 && player.getCurrentHP() > 0){ //iteration
@@ -23,33 +23,30 @@ public class Runner {
                 }
             }
         }
-        if (player.getCurrentHP() > 0) {
-            System.out.println(enemy1.getName() + " is defeated");
-            Enemy enemy2 = new Enemy("Black Slime", 15, 3, 1, 5);
-            AI = "slimeType";
-            System.out.println(enemy2.getName() + " appeared");
-            while (enemy2.getCurrentHP() > 0 && player.getCurrentHP() > 0) {
-                if (enemy2.getSpeed() > player.getSpeed()) {
-                    dmgTaken = enemy2.takeTurn(AI, player.getDefense());
-                    player.setCurrentHP(player.getCurrentHP() - dmgTaken);
-                    if (player.getCurrentHP() > 0) {
-                        dmgTaken = player.takeTurn(enemy2.getDefense(), enemy2.getName());
-                        enemy2.setCurrentHP(enemy2.getCurrentHP() - dmgTaken);
-                    }
-                } else {
+        System.out.println(enemy1.getName() + " is defeated");
+        random = Math.random();
+        Enemy enemy2 = new Enemy("Green Slime", 5, 2, 0, 3);
+        //AI = "slimeType";
+        System.out.println(enemy2.getName() + " appeared");
+        while (enemy2.getCurrentHP() > 0 && player.getCurrentHP() > 0) {
+            if (enemy2.getSpeed() > player.getSpeed()) {
+                dmgTaken = enemy2.takeTurn(AI, player.getDefense());
+                player.setCurrentHP(player.getCurrentHP() - dmgTaken);
+                if (player.getCurrentHP() > 0) {
                     dmgTaken = player.takeTurn(enemy2.getDefense(), enemy2.getName());
                     enemy2.setCurrentHP(enemy2.getCurrentHP() - dmgTaken);
-                    if (enemy1.getCurrentHP() > 0) {
-                        dmgTaken = enemy2.takeTurn(AI, player.getDefense());
-                        player.setCurrentHP(player.getCurrentHP() - dmgTaken);
-                    }
+                }
+            } else {
+                dmgTaken = player.takeTurn(enemy2.getDefense(), enemy2.getName());
+                enemy2.setCurrentHP(enemy2.getCurrentHP() - dmgTaken);
+                if (enemy1.getCurrentHP() > 0) {
+                    dmgTaken = enemy2.takeTurn(AI, player.getDefense());
+                    player.setCurrentHP(player.getCurrentHP() - dmgTaken);
                 }
             }
-            if (player.getCurrentHP() > 0) {
-                System.out.println(enemy1.getName() + " is defeated");
-            } else {
-                System.out.println("player is defeated");
-            }
+        }
+        if (player.getCurrentHP() > 0) {
+            System.out.println(enemy1.getName() + " is defeated");
         } else {
             System.out.println("player is defeated");
         }
